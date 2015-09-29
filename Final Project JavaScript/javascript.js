9385814a242af727532d39f1fbfba2d829c7ea5e
alibati <alibati.com@gmail.com>
2015-09-29 10:43:37 -0400

    Final Project JavaScript

---
+++ Final Project JavaScript/javascript.js
@@ -0,0 +1,14 @@
+var reflectionfeed = new Instafeed ({
+	get: 'tagged',
+	tagName: 'reflection',
+	clientId: '9fbcd71136324de59b090b3d196fa9c0'
+	});
+
+var horizonfeed = new Instafeed ({
+	get: 'tagged',
+	tagName: 'reflection',
+	clientId: '9fbcd71136324de59b090b3d196fa9c0'
+	});
+
+	reflectionfeed.run();
+	horizonfeed.run();
