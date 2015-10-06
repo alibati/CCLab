e495e6194559bc8ef22d7af494e15f990b258a29
Ali Bati <alibati.com@gmail.com>
2015-10-06 00:14:08 -0400

    JS Final

--- Final Project JavaScript/javascript.js
+++ Final Project JavaScript/javascript.js
@@ -1,7 +1,8 @@
 var reflectionfeed = new Instafeed ({
 	get: 'tagged',
 	tagName: 'reflection',
-	clientId: '9fbcd71136324de59b090b3d196fa9c0'
+	clientId: '9fbcd71136324de59b090b3d196fa9c0',
+	limit: '60'
 	});
 
 	reflectionfeed.run();
