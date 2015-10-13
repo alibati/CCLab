5dde5d3e3299546b27b41ffb9284607578c07c19
Ali Bati <alibati.com@gmail.com>
2015-10-13 00:17:53 -0400

    Arduino

--- Week07-Arduino/sketch_oct06e/sketch_oct06e.ino
+++ Week07-Arduino/sketch_oct06e/sketch_oct06e.ino
@@ -6,16 +6,12 @@ int buttonPin2 = 4;
 int buttonState = 0;
 int buttonState2 = 0;
 
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(buttonPin2, INPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
   buttonState = digitalRead(buttonPin);
   buttonState2 = digitalRead(buttonPin2);
@@ -31,7 +27,6 @@ void loop() {
   } else {
    digitalWrite(ledPin2, LOW);
   }
- 
 
  }
 
